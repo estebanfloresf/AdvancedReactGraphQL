3eacd04c4e6adbfbb5268681cb757a1659df427a
Esteban <esteban_fs@hotmail.com>
2018-11-08 21:34:38 -0500

    first query and mutation

---
+++ backend/src/db.js
@@ -0,0 +1,10 @@
+// This files connects to the remote prisma db and gives the ability to query it
+const { Prisma } = require("prisma-binding");
+const db = new Prisma({
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
+  debug: false
+});
+
+module.exports = db;
