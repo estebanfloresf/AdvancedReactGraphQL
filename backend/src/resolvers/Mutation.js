ff6c2205dfa97a15fbb477ac1c60dbcf99e41723
Esteban <esteban_fs@hotmail.com>
2018-11-18 18:30:25 -0500

    Items Creation and Prisma Yoga Flow

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const mutations = {};
+const mutations = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: CHECK IF THEY ARE LOGGED IN
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = mutations;
